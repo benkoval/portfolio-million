893707ebcc584f0ec6d642afd2dce465ab220729
bennykoval <bethanykoval@gmail.com>
2019-09-23 14:33:22 -0400

    Init

---
+++ src/components/Nav.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav>
+        <ul>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Projects</li>
+            <li>LinkedIn</li>
+            <li>GitHub</li>
+            <li>Email</li>
+        </ul>
+    </nav>
+</template>
+
+<style lang='scss'>
+    nav {
+        background: yellow;
+    }
+</style>
