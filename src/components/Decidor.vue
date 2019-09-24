7ebadb6724326b1cfe4a349c9524c140bed9f58a
bennykoval <bethanykoval@gmail.com>
2019-09-23 21:29:19 -0400

    Build projects page

---
+++ src/components/Decidor.vue
@@ -0,0 +1,109 @@
+<template>
+    <section class='decidor-container'>
+        <agile :autoplay='true'>
+            <div class='slide'>
+                <img class='proj-img' src='https://bean-project-screenshots.s3.amazonaws.com/decidor.png'>
+            </div>
+
+            <div class='slide'>
+                <img class='proj-img' src='https://bean-project-screenshots.s3.amazonaws.com/decidor2.png'>
+            </div>
+
+            <div class='slide'>
+                <img class='proj-img' src='https://bean-project-screenshots.s3.amazonaws.com/decidor3.png'>
+            </div>
+
+            <div class='slide'>
+                <img class='proj-img' src='https://bean-project-screenshots.s3.amazonaws.com/decidor4.png'>
+            </div>
+        </agile>
+    </section>
+</template>
+
+<style lang='scss'>
+
+.decidor-container {
+    display: flex;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+}
+.agile {
+    margin: auto;
+    max-width: 40vw;
+}
+.agile__nav-button {
+  background: transparent;
+  border: none;
+  color: #fff;
+  cursor: pointer;
+  font-size: 2rem;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  transition-duration: 0.3s;
+  width: 80px;
+  filter: drop-shadow(2px 1px 1px #222);
+}
+.agile__nav-button:hover {
+//   background-color: rgba(0, 0, 0, 0.5);
+//   opacity: 1;
+  transform: translateY(-.25em);
+}
+.agile__nav-button--prev {
+  left: 0;
+}
+.agile__nav-button--next {
+  right: 0;
+}
+.agile__dots {
+//   bottom: 10px;
+  left: 50%;
+  position: absolute;
+  transform: translateX(-50%);
+//   filter: drop-shadow(2px 1px 1px #222);
+}
+.agile__dot {
+  margin: 0 10px;
+  & button {
+    background-color: transparent;
+    border: 1px solid #0a6cff;
+    border-radius: 50%;
+    cursor: pointer;
+    display: block;
+    height: .8vw;
+    font-size: 0;
+    line-height: 0;
+    margin: 0;
+    padding: 0;
+    transition-duration: 0.3s;
+    width: .8vw;
+    &:hover {
+        background-color: #63A4FF;
+    }
+  }
+}
+.agile__dot--current button {
+  background-color: #63A4FF;
+}
+
+.slide {
+  display: block;
+}
+
+.proj-img {
+    width: 40vw;
+}
+
+</style>
+<script>
+
+import { VueAgile } from 'vue-agile'
+ 
+export default {
+    components: {
+        agile: VueAgile
+    }
+}
+
+</script>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,7 +1,84 @@
 <template>
-    <section class='projects-container' id='projects'>
-        <img src='https://bean-project-screenshots.s3.amazonaws.com/ransom-asanas.png'/>
-        <img src='https://bean-project-screenshots.s3.amazonaws.com/ipreview.png'/>
-        <img src='https://bean-project-screenshots.s3.amazonaws.com/decidor.png'/>
+    <section id='projects'>
+        <div class='projects-container'>
+            <div class='grid-item'>
+                <h3 class='title-proj'>decidOR</h3>
+                <img class='deck-img' src='https://bean-project-screenshots.s3.amazonaws.com/decidor.png'/>
+                <p class='descrip-proj'>See more</p>
+            </div>
+            <div class='grid-item'>
+                <h3 class='title-proj'>iPreview</h3>
+                <img class='deck-img' src='https://bean-project-screenshots.s3.amazonaws.com/ipreview.png'/>
+                <p class='descrip-proj'>See more</p>
+            </div>
+            <div class='grid-item'>
+                <h3 class='title-proj'>Random Asanas</h3>
+                <img class='deck-img' src='https://bean-project-screenshots.s3.amazonaws.com/ransom-asanas.png'/>
+                <p class='descrip-proj'>See more</p>
+            </div>
+            <div class='grid-item'>
+                <h3 class='title-proj'>Calculator</h3>
+                <img class='deck-img' src='https://bean-project-screenshots.s3.amazonaws.com/better-calculator.png'/>
+                <p class='descrip-proj'>See more</p>
+            </div>
+            <div class='grid-item'>
+                <h3 class='title-proj'>Freeshelf</h3>
+                <img class='deck-img' src='https://bean-project-screenshots.s3.amazonaws.com/freeshelf.png'/>
+                <p class='descrip-proj'>See more</p>
+            </div>
+        </div>
     </section>
 </template>
+
+<style lang='scss'>
+$lato: 'Lato', sans-serif;
+$lex: 'Playfair Display', serif;
+
+.projects-container {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(30vw, 4fr));
+    grid-gap: 10px;
+    // grid-auto-rows: minmax(180px, auto);
+    grid-auto-flow: dense;
+    padding: 10px;
+}
+
+.grid-item {
+    margin: auto;
+    padding: 1rem;
+    color: #FFF;
+    background-color: #7C59AF;
+    border-radius: 5px;
+    &:nth-child(odd) {
+        background-color: #3A5BAC;
+    }
+}
+
+.title-proj {
+    text-align: center;
+    font-family: $lex;
+    letter-spacing: 1px;
+    font-size: 2em;
+    text-transform: uppercase;
+}
+
+.deck-img {
+    margin: auto;
+    width: 30vw;
+    border-radius: 5px;
+}
+
+.descrip-proj {
+    font-family: $lato;
+    text-align: center;
+    font-size: 1.1em;
+    &:after {
+        content: '';
+        margin: 0 auto;
+        display: block;
+        width: 17%;
+        border-bottom: .16vw solid;
+        padding-bottom: .3rem;
+    }
+}
+</style>
